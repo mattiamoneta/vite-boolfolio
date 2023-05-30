79b062b499bf121d14804a73f71cc8046ba0bb58
mattiamoneta <in.mattiamoneta@gmail.com>
2023-05-30 17:14:42 +0200

    Final + BONUS

--- src/components/MainPage.vue
+++ src/components/MainPage.vue
@@ -1,29 +1,68 @@
-<template></template>
+<template>
+    <div class="container">
+
+        <div class="row justify-content-center">
+
+            <ProjectCard v-for="(project) in retProjects.data" :arrProject="project" />
+
+
+        </div>
+
+        <div class="row my-5">
+            <div class="col">
+                <nav aria-label="Page navigation example">
+                    <ul class="pagination justify-content-center">
+                        <li class="page-item"><button class="page-link" @click="getAllProjectsInPage(currentPage - 1)"
+                                :class="{ 'disabled': currentPage == 1 }">Previous</button></li>
+                        <li v-for="elem in lastPage" :key="elem" class="page-item" @click="getAllProjectsInPage(elem)">
+                            <button class="page-link">{{ elem }}</button></li>
+                        <li class="page-item"><button class="page-link" @click="getAllProjectsInPage(currentPage + 1)"
+                                :class="{ 'disabled': currentPage >= lastPage }">Next</button></li>
+                    </ul>
+                </nav>
+            </div>
+        </div>
+
+    </div>
+</template>
 
 <script>
 
 import axios from 'axios';
+import ProjectCard from './ProjectCard.vue';
 
 export default {
     name: 'MainPage',
     data() {
         return {
-            retProjects: []
+            retProjects: [],
+            currentPage: 1,
+            lastPage: 0
         }
     },
 
     methods: {
-        getAllProjects() {
-            axios.get('http://127.0.0.1:8000/api/projects')
-                .then(response => {
-                    // this.retProjects = response.data.results;
-                    console.log(response.data.results);
-
+        getAllProjectsInPage(providedPage) {
+            axios.get('http://127.0.0.1:8000/api/projects',
+                {
+                    params: {
+                        page: providedPage
+                    }
                 })
+                .then(response => {
+                    this.retProjects = response.data.results;
+                    this.currentPage = providedPage;
+                    this.lastPage = response.data.results.last_page;
+                })
+
+            console.log(this.lastPage);
         }
     },
+    components: {
+        ProjectCard
+    },
     mounted() {
-        this.getAllProjects();
+        this.getAllProjectsInPage(1);
     }
 }
 
---
+++ src/components/ProjectCard.vue
@@ -0,0 +1,30 @@
+<template>
+    <div class="col-3">
+        <div class="card">
+            <img :src="`http://127.0.0.1:8000/storage/${this.arrProject['thumb']}`" class="card-img-top" alt="...">
+            <div class="card-body">
+                <h5 class="card-title">{{ this.arrProject['name'] }}</h5>
+                <p class="card-text">{{ this.arrProject['description'] }}</p>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'ProjectCard',
+    data() {
+        return {
+
+        }
+    },
+    methods: {
+
+    },
+    props: {
+        arrProject: Object
+    }
+}
+</script>
+
+<style scoped></style>
