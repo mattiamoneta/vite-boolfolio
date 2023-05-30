f0973fb39d5b3e13b3c4ce471a4098f62ea6e25e
mattiamoneta <in.mattiamoneta@gmail.com>
2023-05-30 16:13:01 +0200

    Ok Milestone 4

---
+++ src/components/MainPage.vue
@@ -0,0 +1,33 @@
+<template></template>
+
+<script>
+
+import axios from 'axios';
+
+export default {
+    name: 'MainPage',
+    data() {
+        return {
+            retProjects: []
+        }
+    },
+
+    methods: {
+        getAllProjects() {
+            axios.get('http://127.0.0.1:8000/api/projects')
+                .then(response => {
+                    // this.retProjects = response.data.results;
+                    console.log(response.data.results);
+
+                })
+        }
+    },
+    mounted() {
+        this.getAllProjects();
+    }
+}
+
+
+</script>
+
+<style></style>
