fb2e96b03068b6c4254a74f110cf354b57916cac
mattiamoneta <in.mattiamoneta@gmail.com>
2023-06-06 17:50:59 +0200

    Added Contacts Email Form

--- src/pages/ContactsPage.vue
+++ src/pages/ContactsPage.vue
@@ -4,12 +4,25 @@
             <div class="col-12 mx-auto">
                 <h1 class="text-center">Contacts</h1>
                 <div class="row my-5">
-                    <div class="col-3 mx-auto">
-                        <ul class="list-group">
-                            <li class="list-group-item">Phone: <span class="fw-bold">+39 123 45678912</span></li>
-                            <li class="list-group-item">Mail: <span class="fw-bold">example@mail.it</span></li>
-                            <li class="list-group-item">PEC: <span class="fw-bold">example@pec.it</span></li>
-                        </ul>
+                    <div class="col-10 mx-auto">
+                        <form @submit.prevent="sendEmail()">
+                            <div class="mb-3">
+                                <label for="name" class="form-label">Your Name</label>
+                                <input type="text" class="form-control" id="name" name="name" v-model="name">
+                            </div>
+                            <div class="mb-3">
+                                <label for="email" class="form-label">Email</label>
+                                <input type="email" class="form-control" id="email" name="email" aria-describedby="email"
+                                    v-model="email">
+                            </div>
+                            <div class="mb-3">
+                                <label for="message" class="form-label">Messaggio</label>
+                                <input type="text" class="form-control" id="message" name="message"
+                                    aria-describedby="message" v-model="message">
+                            </div>
+
+                            <button type="submit" class="btn btn-primary">Send Mail</button>
+                        </form>
                     </div>
                 </div>
             </div>
@@ -18,14 +31,26 @@
 </template>
 
 <script>
+
+import axios from 'axios';
+
 export default {
     name: 'ContactsPage',
     data() {
         return {
+            name: '',
+            email: '',
+            message: ' '
 
         }
     },
+    methods: {
+        sendEmail() {
 
+
+        }
+
+    }
 }
 </script>
 
